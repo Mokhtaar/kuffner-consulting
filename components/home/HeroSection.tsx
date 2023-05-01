a1a8d759616627bafe2593caab02c00537862e3f
Mokhtaar <mokh.first@gmail.com>
2023-05-01 17:51:08 +0300

    HeroSectionV1

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -67,7 +67,7 @@ export default function HeroSection() {
         >
           <div className="fixed inset-0 z-50" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
-            <div className="flex items-center justify-between">
+            <div className="flex justify-between">
               <a
                 href="#"
                 className="-m-1.5 mb-8 p-1.5 text-center text-[#0C224A]"
@@ -79,7 +79,7 @@ export default function HeroSection() {
               </a>
               <button
                 type="button"
-                className="-m-2.5 rounded-md p-2.5 text-gray-500"
+                className="-m-2.5 mb-5 rounded-md p-2.5 text-gray-500"
                 onClick={() => setMobileMenuOpen(false)}
               >
                 <span className="sr-only">Close menu</span>
